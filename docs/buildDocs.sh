fc85fb9c49abda01de54ee66aa11c433a00a2352
Robin Long <robin.long1@hotmail.co.uk>
2020-09-24 14:17:54 +0100

    using static dir instead of mktmp

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -30,7 +30,7 @@ ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # make a new temp dir which will be our GitHub Pages docroot
-docroot=`mktemp -d`
+docroot=`mkdir /tmp/docs`
 
 export REPO_NAME="${GITHUB_REPOSITORY##*/}"
  
